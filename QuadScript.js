7df37e292f4404faf04926a112453e5e31179ce5
samsakaguchi <35707434+samsakaguchi@users.noreply.github.com>
2018-02-05 13:28:15 -0800

    Add files via upload

--- QuadScript.js
+++ QuadScript.js
@@ -8,6 +8,7 @@ function init() {
   h = canvas.height = 400;
   console.log('canvas is loaded into context',w);
   graphpaper();
+  graphQuad();
 }  // close init
 
 function QF() {
@@ -27,6 +28,8 @@ function QF() {
   if (x2<0) {
     x2 = x2*(-1)+"i";
   }
+  xcp = 2*vX;
+
   $("#solution1").text("X intercept is at "+x1);
   $("#solution2").text("X intercept is at "+x2);
   $("#y-int1").text("Y intercept is at (0,"+ c+ ")");
@@ -41,6 +44,11 @@ function results() {
   // finding vertex and displaying symline and yint results
 
   $("#vertex").text("Vertex is at (" + vX+","+vY+")");
+  $("#core").text("Corresponding Point  is at (" +xcp+","+c+")");
+  context.beginPath();
+  context.arc(200, c, 5, 0, 2 * Math.PI);
+  context.stroke();
+  $("#aos").text("Symmetry Line is at " +vX);
 }  // close results()
 
 function graphpaper() {
@@ -49,8 +57,6 @@ function graphpaper() {
   context.moveTo(w/2, 0);
   context.lineTo(w/2, h);
   context.stroke();
-
-
   context.beginPath();
   context.moveTo(0, h/2);
   context.lineTo(w, h/2);
@@ -74,37 +80,42 @@ context.strokeStyle="rgba(0,0,255,.5)";
     context.moveTo( 0, h/2-i*-k );
     context.lineTo( w, h/2-i*-k );
     context.stroke();
- 
-}
+
+  }
   for (i=0; i<w/(2*k); i++) {
     context.beginPath();
     context.moveTo( w/2-i*k, 0 );
     context.lineTo( w/2-i*k, h);
     context.stroke();
-   
+
     context.beginPath();
     context.moveTo( w/2-i*-k, 0);
     context.lineTo( w/2-i*-k, h);
     context.stroke();
- 
-}
-}
-function k (){
-  k+3
+
+  }
 }
 
 function graphQuad () {
   context.lineWidth = 1;
   context.strokeStyle = "Red";
   for (var i = 0; i < w; i++) {
-    x = (w/2-i/k);
+    x = (w/2-i)/k;
     y = c*1+b*x+a*Math.pow(x,2);
     nx = (w/2-(i+1))/k;
     ny = c*1+b*nx+a*Math.pow(nx,2);
-    console.log(x,y,nx,ny);
     context.beginPath();
     context.moveTo(w/2+x*k,h/2-y*k);
     context.lineTo(w/2+nx*k,h/2-ny*k);
     context.stroke();
   }
+}
+
+function maximize(){ 
+  k = k+3;
+  init();
+}
+function minimize(){
+  k = k-3; 
+  init();
 }
