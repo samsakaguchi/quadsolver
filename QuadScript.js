e4ab777d5499970e72b84199485e675dace0c934
samsakaguchi <35707434+samsakaguchi@users.noreply.github.com>
2018-02-27 13:29:41 -0800

    #4

--- QuadScript.js
+++ QuadScript.js
@@ -7,8 +7,11 @@ function init() {
   w = canvas.width = 600;
   h = canvas.height = 400;
   console.log('canvas is loaded into context',w);
+  canvasOffset = canvas.getBoundingClientRect();
+  offsetX = Math.round(canvasOffset.left);
+  offsetY = Math.round(canvasOffset.top); 
+  canvas.addEventListener("mousemove", doMouseMove, false);
   graphpaper();
-  graphQuad();
 }  // close init
 
 function QF() {
@@ -16,6 +19,20 @@ function QF() {
   a = $("#quadA").val();
   b = $("#linB").val();
   c = $("#constant").val();
+  d= b**2-4*a*c;  
+  if(d<0){
+    $("#solution1").text("No x-intercepts");
+    $("#solution2").text("");
+  }
+  else{
+ x1 = (-b+Math.sqrt(b**2-4*a*c))/(2*a);
+  x1 = Math.round(x1*1000)/1000;
+  x2 = (-b-Math.sqrt(b**2-4*a*c))/(2*a);
+  x2 = Math.round(x2*1000)/1000;
+  $("#solution1").text("X intercept is at "+x1);
+  $("#solution2").text("X intercept is at "+x2);
+  }
+
   x1 = (-b+Math.sqrt(b**2-4*a*c))/(2*a);
   x1 = Math.round(x1*1000)/1000;
   x2 = (-b-Math.sqrt(b**2-4*a*c))/(2*a);
@@ -26,20 +43,33 @@ function QF() {
   console.log(a,b,c); 
   vX = vX.toFixed(2);
   vY = vY.toFixed(2);
-
   graphQuad();
   results();
 }  // close QF
 
 function results() {
+if (a==0){
+ $("#vertex").text("No Vertex");
+ $("#solution1").text("X-intercept is at (" + -c/b +","+0+")");
+ $("#solution2").text("");
+ $("#core").text("No Corresponding Point");
+ $("#y-int1").text("Y intercept is at "+c+"");
+ $("#aos").text("No Symmetry Line");
 
-  // finding vertex and displaying symline and yint results
-  $("#solution1").text("X intercept is at "+x1);
+ context.fillStyle = "darkblue";
+  context.beginPath();
+  context.arc(w/2+(-c/b)*k, h/2, 4, 0,6.28);
+  context.fill();
+    
+  context.beginPath();
+  context.arc(w/2, h/2-c*k, 4, 0, 6.28);
+  context.fill();
+}
+else{
   context.fillStyle = "darkblue"
   context.beginPath();
   context.arc(w/2+x1*k, h/2, 4, 0,6.28);
   context.fill();
-  $("#solution2").text("X intercept is at "+x2);
   context.fillStyle = "darkblue"
   context.beginPath();
   context.arc(w/2+x2*k, h/2, 4, 0,6.28);
@@ -67,6 +97,7 @@ function results() {
   context.lineTo(w/2+vX*k, h+5)
   context.stroke();
   context.setLineDash([0]);
+}
 }  // close results()
 
 function graphpaper() {
@@ -130,14 +161,38 @@ function graphQuad () {
 
 function maximize(){ 
   k = k+2;
-  init();
-  QF();
+  resetCanvas();
 }
 function minimize(){
   k = k-2; 
+  if (k<10){
+    k=k+2    
+  };
+  resetCanvas();
+}
+function Clear() {
   init();
+}
+function resetCanvas() {
+  init();
+  graphQuad();
+  results();
   QF();
 }
-function clear(){
-  context.clear();
-}
+
+function doMouseMove(event) {
+    // always know where ther mouse is located
+  resetCanvas();
+  context.fillStyle = "red";
+  mouseX = event.clientX-offsetX;
+  mouseY = event.clientY-offsetY;
+  pointX = (mouseX-w/2)/k;
+  pointY = a*Math.pow(pointX,2)+b*pointX+c*1;
+  pointX =  pointX.toFixed(2);
+  pointY =  pointY.toFixed(2);
+  //console.log(mouseX,mouseY, pointX, pointY, offsetY, offsetX);
+  context.beginPath();
+  context.arc(mouseX, h/2-pointY*k,5,0,2*Math.PI);
+  context.fill(); 
+  $("#point").text("Point on the curve: ("+pointX+","+pointY+")");
+}  // end doMouseMovedd
